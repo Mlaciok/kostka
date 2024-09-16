8a71af6449d75b585ff9891397091238eb2e9575
Mlaciok <rozalewski.gd@gmail.com>
2024-09-16 10:21:32 +0200

    Dodaj pliki projektów.

---
+++ cos/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace cos
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ cos/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace cos
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            Random r = new Random();
+            if (kostka4.IsChecked == true)
+            {
+                int roll = r.Next(1, 5);
+                RollValueLabel.Text = roll.ToString();
+            }
+            else if (kostka6.IsChecked == true)
+            {
+                int roll = r.Next(1, 7);
+                RollValueLabel.Text = roll.ToString();
+            }
+        }
+    }
+}
